eed20da70dfbe6b72baa7c95e443b86b7f24c35f
misostack <misostack.com@gmail.com>
2021-10-28 00:26:11 +0700

    add monitoring tool

--- README.md
+++ README.md
@@ -18,3 +18,27 @@
 - https://www.guru99.com/sql.html
 - https://www.w3schools.com/MySQL/default.asp
 - https://dev.mysql.com/doc/refman/8.0/en/optimization.html
+- https://www.javatpoint.com/mysql-select-database
+
+```sql
+CREATE USER 'exporter'@'localhost' IDENTIFIED BY '12345678' WITH MAX_USER_CONNECTIONS 3;
+GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'localhost';
+cd monitoring
+.\mysqld_exporter.exe --config.my-cnf .\.my.cnf
+
+.\prometheus.exe --config.file .\prometheus.yml --web.listen-address="0.0.0.0:9105"
+```
+
+```yml
+# config file for monitoring MySQL via mysqld_exporter on localhost
+global:
+  scrape_interval: 5s # Set the scrape interval to every 15 seconds. Default is every 1 minute.
+  evaluation_interval: 5s # Evaluate rules every 15 seconds. The default is every 1 minute.
+
+scrape_configs:
+  - job_name: "mysql-monitor"
+    static_configs:
+      - targets: ["localhost:9104"]
+        labels:
+          alias: db1
+```
--- phpguru-headfirst.sql
+++ phpguru-headfirst.sql
@@ -17,3 +17,46 @@ CREATE TABLE IF NOT EXISTS `phpguru_users`
   is_super_admin BOOLEAN NULL DEFAULT 0,
   PRIMARY KEY (id)
 );
+
+ALTER TABLE `phpguru_users`
+ADD CONSTRAINT UC_User_Email UNIQUE(email);
+
+ALTER TABLE `phpguru_users`
+ADD CONSTRAINT UC_User_Login UNIQUE(login);
+
+SHOW INDEX FROM `phpguru_users`;
+
+ALTER TABLE `phpguru_users`
+ADD CONSTRAINT CKH_User_IsSuperAdmin CHECK (is_super_admin IN(0,1));
+
+SHOW COLUMNS FROM `phpguru_users`;
+
+SHOW CREATE TABLE `phpguru_users`;
+
+ALTER TABLE `phpguru_users`
+ADD COLUMN gender CHAR(1) NOT NULL;
+
+ALTER TABLE `phpguru_users`
+ADD CONSTRAINT users_chk_gender
+CHECK(
+  CASE
+    WHEN gender = 'M' THEN 1
+    WHEN gender = 'm' THEN 1
+    WHEN gender = 'F' THEN 1
+    WHEN gender = 'f' THEN 1
+    ELSE 0
+  END = 1
+);
+
+ALTER TABLE `phpguru_users`
+ADD CONSTRAINT users_chk_first_name
+CHECK(1=1);
+
+SELECT
+       CONSTRAINT_NAME,
+       TABLE_SCHEMA,
+       TABLE_NAME
+       FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+       WHERE TABLE_NAME = 'phpguru_users'
+       AND CONSTRAINT_SCHEMA = 'phpguru-headfirst'
+       AND  CONSTRAINT_TYPE = 'CHECK';
